17521e693709122bb2b302e87148d2292ec9d9cf
Xxianna <xiaoxianxianna01@outlook.com>
2025-01-27 09:55:50 +0800

    加入帧率显示，关闭垂直同步

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import moderngl_window as mglw
-
+from moderngl_window.text.bitmapped import TextWriter2D
 
 class App(mglw.WindowConfig):
-    window_size = 1600, 900
+    window_size = 1280, 720
     resource_dir = 'resources'
+    vsync = False
+    # fullscreen = True
+    # hidden_window_framerate_limit = 900000
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -24,17 +27,24 @@ class App(mglw.WindowConfig):
         self.set_uniform('u_texture3', 3)
         self.texture3.use(location=3)
 
+        self.tttimer = self.timer
+        self.writer = TextWriter2D()
+        self.writer.text = "Hello ModernGL!"
+
+
     def set_uniform(self, u_name, u_value):
         try:
             self.program[u_name] = u_value
         except KeyError:
             print(f'{u_name} not used in shader')
 
-    def render(self, time, frame_time):
+    def on_render(self, time, frame_time):
         self.ctx.clear()
         self.set_uniform('u_time', time)
         self.quad.render(self.program)
 
+        self.writer.text = f"FPS: {self.tttimer.fps_average:.2f}"
+        self.writer.draw((200, 200), size=30)
 
 if __name__ == '__main__':
     mglw.run_window_config(App)
